95f4b23fa2a2b07a58f2b1b31d26b15ad35653a0
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-08 01:40:58 +0800

    setup env

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+module.exports = {
+  entry: {
+    main: "./src/index.js",
+    vendor: "./src/vendor.js",
+  },
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[contenthash].[ext]",
+            outputPath: "assets/images",
+            publicPath: "assets/images",
+          },
+        },
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
