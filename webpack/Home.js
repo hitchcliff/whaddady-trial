8d5a2e40163a037e8972797825320141b482da02
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-08 06:16:24 +0800

    arranged the folder structure for partials

--- webpack/Home.js
+++ webpack/Home.js
@@ -4,7 +4,7 @@ const HtmlWebpackPartialPlugin = require("html-webpack-partials-plugin");
 module.exports = {
   plugins: [
     new HtmlWebpackPartialPlugin({
-      path: path.resolve(__dirname, "../src/views/Home/partials/hero-section.html"),
+      path: path.resolve(__dirname, "../src/views/home-partials/hero-section.html"),
       location: "HeroSection",
       template_filename: ["index.html"],
     }),
