aab9fc04635a55c1db434a887a1ed61ae76a58ed
Kevin Nacario <hitchcliff1@gmail.com>
2021-01-10 19:52:27 +0800

    edited a bit in hero-section

--- src/app/Home/Parallax.js
+++ src/app/Home/Parallax.js
@@ -1,39 +1,50 @@
 import * as ScrollMagic from "scrollmagic";
+import $ from "jquery";
 import { TimelineLite } from "gsap";
 
 export default function HomeParallax() {
   const controller = new ScrollMagic.Controller();
 
   let tl = new TimelineLite();
+  let tl2 = new TimelineLite();
+  const hero = $("#hero-image");
+  const bg = $("#bg-image");
+  const logo = $("#hero-logo");
+  const social = $("#hero-social");
 
-  tl.to("#hero-image", 3, {
-    y: -600,
-  })
+  tl.to(
+    bg,
+    3,
+    {
+      y: -600,
+    },
+    "-=.5",
+  )
     .to(
-      "#hero-logo",
+      logo,
       3,
       {
-        y: -400,
+        y: -0,
       },
       "=-3",
     )
     .to(
-      "#hero-social",
+      social,
       3,
       {
-        y: -390,
+        y: -0,
         opacity: 1,
       },
       "=-3",
-    )
-    .to(
-      ".blur, .hero",
-      3,
-      {
-        top: "0%",
-      },
-      "=-3",
     );
+  // .to(
+  //   hero,
+  //   3,
+  //   {
+  //     y: -50,
+  //   },
+  //   "-=3",
+  // );
 
   new ScrollMagic.Scene({
     triggerElement: "#main-body",
@@ -42,5 +53,26 @@ export default function HomeParallax() {
   })
     .setPin("#main-body")
     .setTween(tl)
+    .addTo(controller);
+
+  tl2
+    .from(bg, 1, {
+      y: -100,
+      scale: 1.5,
+    })
+    .from(
+      hero,
+      1,
+      {
+        y: 150,
+      },
+      "-=1",
+    );
+
+  new ScrollMagic.Scene({
+    triggerElement: "#main-body",
+    triggerHook: "onLeave",
+  })
+    .setTween(tl2)
     .addTo(controller);
 }
